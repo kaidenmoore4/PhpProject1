55efb13b26e62484050a152fe57d54993f12e858
kaidenmoore4 <43053428+kaidenmoore4@users.noreply.github.com>
2018-09-28 22:57:25 -0400

    updated on github

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "this is project 12";
+        echo "this is project 13";
         ?>
     </body>
 </html>
